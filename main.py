c5e95c3167e9f83182010f586d53237879b620c4
Ganesh Chaudhary <trumpnp72@gmail.com>
2024-05-24 15:12:35 +0545

    second commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/health")
+def health():
+    return render_template("health.html")
+
+@app.route("/electronics")
+def electronics():
+    return render_template("electronics.html")
+@app.route("/cloths")
+def cloths():
+    return render_template("cloths.html")
+@app.route("/homestyle")
+def homestyle():
+    return render_template("homestyle.html")
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
